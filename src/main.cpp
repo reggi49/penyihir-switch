3238541257ed5e18a16d11623157b1f143c05372
reggi49 <reggimuhamad@gmail.com>
2023-07-10 16:05:09 +0700

    bug fixing

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 using namespace c2d;
 namespace fs = std::filesystem;
+bool isKeyPressed = false;
 
 IntRect getTextureRect(Texture *texture, int direction)
 {
@@ -42,6 +43,13 @@ IntRect getTextureRect(Texture *texture, int direction)
 //     renderer->add(showResults);
 // }
 
+void processKeyPress()
+{
+    // std::cout << "Key pressed!" << std::endl;
+    // printf("Key pressed!");
+    isKeyPressed = true;
+}
+
 void copyFile(const fs::path &sourcePath, const fs::path &destinationPath)
 {
     std::ifstream sourceFile(sourcePath, std::ios::binary);
@@ -60,6 +68,16 @@ void copyFile(const fs::path &sourcePath, const fs::path &destinationPath)
 
 void copyDirectory(const fs::path &sourcePath, const fs::path &destinationPath)
 {
+    // if (fs::exists(destinationPath))
+    // {
+    //     fs::remove_all(destinationPath);
+    //     // std::cout << "Folder deleted successfully." << std::endl;
+    // }
+    // else
+    // {
+    //     fs::create_directories(destinationPath);
+    // }
+
     fs::create_directories(destinationPath);
 
     for (const auto &entry : fs::directory_iterator(sourcePath))
@@ -76,11 +94,12 @@ void copyDirectory(const fs::path &sourcePath, const fs::path &destinationPath)
             copyFile(sourceFilePath, destinationFilePath);
         }
     }
+    std::cout << "Copy completed " << std::endl;
 }
 
 int main(int argc, char **argv)
 {
-    // char fps[64];
+    char fps[64];
 
     // create the main renderer
     auto *renderer = new C2DRenderer();
@@ -91,7 +110,7 @@ int main(int argc, char **argv)
     //                           renderer->getSize().x * (20 * m_scaling.x), renderer->getSize().y * (10 * m_scaling.y)});
     // bg->setOrigin(Origin::Bottom);
     // gameView->add(bg);
-
+ 
     // bg with texture
     auto *texture = new C2DTexture(renderer->getIo()->getRomFsPath() + "bg.jpeg");
     texture->setOrigin(Origin::Center);
@@ -100,30 +119,48 @@ int main(int argc, char **argv)
     renderer->add(texture);
 
     // create a buttons texture for the sprite
-    auto buttons = new C2DTexture(renderer->getIo()->getRomFsPath() + "buttons.png");
+    // auto buttons = new C2DTexture(renderer->getIo()->getRomFsPath() + "buttons.png");
 
     // create a sprite
-    auto sprite = new Sprite(buttons, getTextureRect(buttons, 0));
-    sprite->setOrigin(Origin::Center);
-    sprite->setPosition(renderer->getSize().x / 2, renderer->getSize().y / 2);
-    sprite->setVisibility(Visibility::Visible);
-    renderer->add(sprite);
+    // auto sprite = new Sprite(buttons, getTextureRect(buttons, 0));
+    // sprite->setOrigin(Origin::Center);
+    // sprite->setPosition(renderer->getSize().x / 2, renderer->getSize().y / 2);
+    // sprite->setVisibility(Visibility::Visible);
+    // renderer->add(sprite);
 
-    auto text = new Text("Press (A) to Change The Language", 50);
-    text->setOrigin(Origin::Center);
-    text->setFillColor(Color::Red);
-    text->setOutlineColor(Color::White);
-    text->setOutlineThickness(2);
-    text->setPosition(sprite->getPosition().x, sprite->getPosition().y - 50);
-    text->setVisibility(Visibility::Visible);
-    renderer->add(text);
+    auto changeLanguage = new Text("Press (A) to Change The Language", 50);
+    changeLanguage->setOrigin(Origin::Center);
+    changeLanguage->setFillColor(Color::Red);
+    changeLanguage->setOutlineColor(Color::White);
+    changeLanguage->setOutlineThickness(2);
+    changeLanguage->setPosition(renderer->getSize().x / 2, renderer->getSize().y / 2);
+    changeLanguage->setVisibility(Visibility::Visible);
+    renderer->add(changeLanguage);
 
-    auto successText = new Text("!!! Success Change Language to Indonesian !!!", 50);
+    auto cancelLanguage = new Text("Press (B) to Cancel", 50);
+    cancelLanguage->setOrigin(Origin::Center);
+    cancelLanguage->setFillColor(Color::Red);
+    cancelLanguage->setOutlineColor(Color::White);
+    cancelLanguage->setOutlineThickness(2);
+    cancelLanguage->setPosition(renderer->getSize().x / 2, renderer->getSize().y / 2 + 70);
+    cancelLanguage->setVisibility(Visibility::Visible);
+    renderer->add(cancelLanguage);
+
+    auto processingLanguage = new Text("Patching Language ...", 50);
+    processingLanguage->setOrigin(Origin::Center);
+    processingLanguage->setFillColor(Color::Red);
+    processingLanguage->setOutlineColor(Color::White);
+    processingLanguage->setOutlineThickness(2);
+    processingLanguage->setPosition(changeLanguage->getPosition().x, changeLanguage->getPosition().y - 50);
+    processingLanguage->setVisibility(Visibility::Hidden);
+    renderer->add(processingLanguage);
+
+    auto successText = new Text("!!! Success Change Language to Indonesian, Press B to Closes !!!", 50);
     successText->setOrigin(Origin::Center);
     successText->setFillColor(Color::Red);
     successText->setOutlineColor(Color::White);
     successText->setOutlineThickness(2);
-    successText->setPosition(sprite->getPosition().x, sprite->getPosition().y - 30);
+    successText->setPosition(renderer->getSize().x / 2, renderer->getSize().y / 2);
     successText->setVisibility(Visibility::Hidden);
     renderer->add(successText);
 
@@ -134,30 +171,51 @@ int main(int argc, char **argv)
     credit->setPosition(renderer->getSize());
     renderer->add(credit);
 
+    auto *text = new Text("FPS: 60/60");
+    renderer->add(text);
+
     // main loop
     while (true)
     {
         unsigned int keys = renderer->getInput()->getButtons();
-        // exit if START or SELECT is pressed (+/- on switch)
-        if (keys & Input::Button::Start || keys & Input::Button::Select)
+        // exit if START or SELECT or B is pressed (+/- on switch)
+        if (keys & Input::Button::Start || keys & Input::Button::Select || keys & Input::Button::B)
         {
             break;
         }
 
-        if (keys & Input::Button::A)
+        if (!isKeyPressed)
         {
-            text->setVisibility(Visibility::Hidden);
-            sprite->setVisibility(Visibility::Hidden);
+            if (keys & Input::Button::A)
+            {
+                processKeyPress();
+                changeLanguage->setVisibility(Visibility::Hidden);
+                cancelLanguage->setVisibility(Visibility::Hidden);
+                // processingLanguage->setVisibility(Visibility::Visible, true);
 
-            copyDirectory(sourcePath, destinationPath);
-            successText->setVisibility(Visibility::Visible, true);
-            
-            // showResults();
+                const fs::path sourcePath = "romfs:/0100BFE00E9CA000";
+                const fs::path destinationPath = "/atmosphere/contents/0100BFE00E9CA000";
+
+                // sprite->setVisibility(Visibility::Hidden);
+
+                copyDirectory(sourcePath, destinationPath);
+
+                // processingLanguage->setVisibility(Visibility::Hidden, true);
+                successText->setVisibility(Visibility::Visible, true);
+                // showResults();
+            }
         }
+            // isKeyPressed = false;
 
-        // draw everything
-        renderer->flip();
-    }
+            snprintf(fps, 65, "FPS: %.2g/60\nFreeze means patching is in progress : %f",
+                     renderer->getFps(),
+                     renderer->getDeltaTime()
+                         .asSeconds());
+            text->setString(fps);
+
+            // draw everything
+            renderer->flip();
+        }
 
     // cleanup
     // will delete child's (textures, shapes, text..)
